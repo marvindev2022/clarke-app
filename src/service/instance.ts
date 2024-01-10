0951089b45fea873abc2f101a5e44ebe953aa3f2
Marcus  Roza <mavirolero@gmail.com>
2024-01-10 16:38:36 -0300

    Update base URL to use production API

--- src/service/instance.ts
+++ src/service/instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api-clarke-energia.onrender.com",
   timeout: 10000,  
   headers: {
     'Content-Type': 'application/json',
