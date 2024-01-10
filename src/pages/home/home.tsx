ba7df758c714faaa008fbf9e4bbeed96720bb9b3
Marcus  Roza <mavirolero@gmail.com>
2024-01-10 16:32:16 -0300

    Add home page component

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+import RenderHeader from "../../components/Header";
+import RenderTable from "../../components/Table";
+
+export default function RenderHome() {
+  return (
+    <section className="w-screen h-screen ">
+     <RenderHeader />
+      <RenderTable />
+    </section>
+  );
+}
