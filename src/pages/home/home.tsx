fc35ef9d2cf94cf220620208d93c9fcc97519ab7
Marcus  Roza <mavirolero@gmail.com>
2024-01-10 16:59:41 -0300

    Add online status check in home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState, useRef } from "react";
 import RenderHeader from "../../components/Header";
 import RenderTable from "../../components/Table";
+import instance from "../../service/instance";
+import { promiseToast } from "../../utils/notifications";
 
 export default function RenderHome() {
+  const [online, setOnline] = useState(false);
+  const hasEffectRun = useRef(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { status } = await promiseToast(
+          instance.get("/supplier/find"),
+          "Ligando ao servidor",
+          "Conectado ao servidor"
+        );
+
+        if (status < 400 && !online) {
+          setOnline(true);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    if (!hasEffectRun.current) {
+      fetchData();
+      hasEffectRun.current = true;
+    }
+  }, []);
+
   return (
-    <section className="w-screen h-screen ">
-     <RenderHeader />
+    <section className="w-screen h-screen">
+      <RenderHeader />
       <RenderTable />
     </section>
   );
